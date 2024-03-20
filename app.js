9156d989bd4dbf4498a61196e62cc718ba20feaa
DamirXolboyev <104071903+DamirXolboyev@users.noreply.github.com>
2024-03-20 18:56:15 +0500

    uzgarish

---
+++ app.js
@@ -0,0 +1,68 @@
+ const PRODUCTS =[
+    {
+        p:'Salim',
+        p1: '(12) отзывов',
+        p3: '  Вариативный замок Golden Soft для отеля',
+        p4: '7 000₽',
+        p5:'8 000₽',
+        img: "https://johnlewis.scene7.com/is/image/JohnLewis/mobiles-nav-card-apple2-200323"
+    },
+    {
+        p:'SALE',
+        p1: '(12) отзывов',
+        p3: '  Вариативный замок Golden Soft для отеля',
+        p4: '7 000₽',
+        p5:'8 000₽',
+        img: "https://assets-global.website-files.com/610ddb83766d492e4422c09f/62f5e3f37ff2dd022c10ec17_Black-iPhone%201%20(1).webp"
+    },
+    {
+        p:'SALE',
+        p1: '(12) отзывов',
+        p3: '  Вариативный замок Golden Soft для отеля',
+        p4: '7 000₽',
+        p5:'8 000₽',
+        img: "https://media.ao.com/en-GB/Productimages/Images/rvMedium/6ayy8pwwcb3bko3h7hwx4j-iphone_12_black_mobi_m_p.jpg"
+    },
+    {
+        p:'SALE',
+        p1: '(12) отзывов',
+        p3: '  Вариативный замок Golden Soft для отеля',
+        p4: '7 000₽',
+        p5:'8 000₽',
+        img: "https://target.scene7.com/is/image/Target/GUEST_ff3c311a-e99e-4834-bac4-07055eb95f5d?qlt=65&fmt=pjpeg&hei=350&wid=350"
+    },
+    {
+        p:'SALE',
+        p1: '(12) отзывов',
+        p3: '  Вариативный замок Golden Soft для отеля',
+        p4: '7 000₽',
+        p5:'8 000₽',
+        img: "https://my.priceshop.com/imgps/image/b3182f3497e24c3e9b0d967c59c8cb193"
+    },
+    {
+        p:'SALE',
+        p1: '(12) отзывов',
+        p3: '  Вариативный замок Golden Soft для отеля',
+        p4: '7 000₽',
+        p5:'8 000₽',
+        img: "https://johnlewis.scene7.com/is/image/JohnLewis/mobiles-nav-card-apple2-200323"
+    },
+    {
+        p:'SALE',
+        p1: '(12) отзывов',
+        p3: '  Вариативный замок Golden Soft для отеля',
+        p4: '7 000₽',
+        p5:'8 000₽',
+        img: "https://johnlewis.scene7.com/is/image/JohnLewis/mobiles-nav-card-apple2-200323"
+    },
+    {
+        p:'SALE',
+        p1: '(12) отзывов',
+        p3: '  Вариативный замок Golden Soft для отеля',
+        p4: '7 000₽',
+        p5:'8 000₽',
+        img: "https://johnlewis.scene7.com/is/image/JohnLewis/mobiles-nav-card-apple2-200323"
+    },
+    
+]
+export {PRODUCTS}
--- main.js
+++ main.js
@@ -1,5 +1,158 @@
+import { PRODUCTS } from './app.js'
+console.log(PRODUCTS);
+
 const logoBtn = document.querySelector('.menu')
 const nav = document.querySelector('.botA')
 logoBtn.addEventListener('click',()=>{
     nav.classList.toggle('google')
 })
+
+function createTable(data){
+    const div =document.querySelector('.hammasi').innerHTML = link
+    while(div.firstChild){
+        div.firstChild.remove()
+    }
+    data.forEach((user)=>{
+      let tr = document.createElement('tr')
+      tr.innerHTML = `
+
+      <section class="hammasi">
+      <div class="container">
+      <div class="all">
+          <div class="all-row">
+              <div class="hammasi-all">
+                 ${user.img}
+                  </div>
+                  <div class="hammasi-all">
+                      <p>${user.p}</p>
+                      </div>
+</div>
+<div class="img">
+  <img src="./img/fon.webp" alt="">
+</div>
+      </div>
+      <div class="bottom">
+          <div class="bottom-all">
+              <div class="bottom-row">
+                  <img src="./img/rasm2.png" alt="">
+               </div>
+               <div class="bottom-row">
+                 <p>${user.p1}</p>
+               </div>
+          </div>
+          <p class="p-1" >
+             ${user.p3}
+          </p>
+          <div class="teg">
+              <div class="p-2">
+                  <p>${user.p4}</p>
+              </div>  
+              <div class="p-2">
+                  <p class="p-3">${user.p5}</p>
+              </div>
+          </div>
+      </div>
+  </div>
+  </section>
+      
+      `
+      div.appendChild(tr)
+    })
+    createTable(PRODUCTS)
+  }
+
+
+  let link = ''
+  let javob = `
+  <section class="hammasi">
+  <div class="container">
+  <div class="all">
+      <div class="all-row">
+          <div class="hammasi-all">
+              <img src="./img/rasm1.png" alt="">
+              </div>
+              <div class="hammasi-all">
+                  <p>SALE</p>
+                  </div>
+</div>
+<div class="img">
+<img src="./img/fon.webp" alt="">
+</div>
+  </div>
+  <div class="bottom">
+      <div class="bottom-all">
+          <div class="bottom-row">
+              <img src="./img/rasm2.png" alt="">
+           </div>
+           <div class="bottom-row">
+             <p>(12) отзывов</p>
+           </div>
+      </div>
+      <p class="p-1" >
+          Вариативный замок Golden Soft для отеля
+      </p>
+      <div class="teg">
+          <div class="p-2">
+              <p>7 000₽</p>
+          </div>  
+          <div class="p-2">
+              <p class="p-3">8 000₽</p>
+          </div>
+      </div>
+  </div>
+</div>
+</section>
+  `
+for(let i =createTable = 1 ; i<= 8; i++){
+    link+=javob
+}
+const div =document.querySelector('.hammasi').innerHTML = link
+
+
+
+
+
+// let list = ''
+// let arry = `
+// <section class="hammasi">
+// <div class="container">
+// <div class="all">
+//     <div class="all-row">
+//         <div class="hammasi-all">
+//             <img src="./img/rasm1.png" alt="">
+//             </div>
+//             <div class="hammasi-all">
+//                 <p>SALE</p>
+//                 </div>
+// </div>
+// <div class="img">
+// <img src="./img/fon.webp" alt="">
+// </div>
+// </div>
+// <div class="bottom">
+//     <div class="bottom-all">
+//         <div class="bottom-row">
+//             <img src="./img/rasm2.png" alt="">
+//          </div>
+//          <div class="bottom-row">
+//            <p>(12) отзывов</p>
+//          </div>
+//     </div>
+//     <p class="p-1" >
+//         Вариативный замок Golden Soft для отеля
+//     </p>
+//     <div class="teg">
+//         <div class="p-2">
+//             <p>7 000₽</p>
+//         </div>  
+//         <div class="p-2">
+//             <p class="p-3">8 000₽</p>
+//         </div>
+//     </div>
+// </div>
+// </div>
+// </section>`
+// for(let i =1; i<= 8; i++){
+//     list +=arry
+// }
+// document.querySelector('.hammasi').innerHTML = list
